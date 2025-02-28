792c17f0a028337cf897159478dad7727ec3cf1c
tetaud-sebastien <tetaud.sebastien@gmail.com>
2025-02-28 19:39:00 +0000

    experiment Xbatcher for streaming training

--- data/loaders.py
+++ data/loaders.py
@@ -38,7 +38,7 @@ def load_data(config):
     hr_data = hr_data[config["dataset"]["data_target"]]
     hr_data = hr_data.astype("float32")
     hr = hr_data.sel(latitude=slice(latitude_range[0],latitude_range[1]),
-                    longitude=slice(longitude_range[0],longitude_range[1]))
+                    longitude=slice(longitude_range[0],longitude_range[1]),)
     hr = hr.sel(time=config["dataset"]["time_range"])
 
     hr.attrs["units"] = config["dataset"]["unit"]
